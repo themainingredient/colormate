7f23902fe3b2784d9f27eb0247d49341599ae6d9
jeroennknol <jeroen-knol@live.nl>
2019-03-25 12:34:37 +0100

    Initial commit (Webview running React)

---
+++ resources/webview.js
@@ -0,0 +1,15 @@
+import './app';
+
+// Disable the context menu to have a more native feel
+document.addEventListener("contextmenu", function(e) {
+  e.preventDefault();
+});
+
+document.getElementById('button').addEventListener('click', function () {
+  window.postMessage('nativeLog', 'Called from the webview')
+})
+
+// called from the plugin
+window.setRandomNumber = function (randomNumber) {
+  document.getElementById('answer').innerHTML = 'Random number from the plugin: ' + randomNumber
+}
---
+++ src/my-command.js
@@ -0,0 +1,36 @@
+import BrowserWindow from 'sketch-module-web-view'
+const UI = require('sketch/ui')
+
+export default function() {
+  const options = {
+    identifier: 'unique.id',
+    width: 240,
+    height: 180,
+    show: true,
+    resizable: false,
+  }
+
+  var browserWindow = new BrowserWindow(options)
+
+  // only show the window when the page has loaded
+  browserWindow.once('ready-to-show', () => {
+    browserWindow.show()
+  })
+
+  const webContents = browserWindow.webContents
+
+  // print a message when the page loads
+  webContents.on('did-finish-load', () => {
+    UI.message('UI loaded!')
+  })
+
+  // add a handler for a call from web content's javascript
+  webContents.on('nativeLog', (s) => {
+    UI.message(s)
+    webContents
+      .executeJavaScript(`setRandomNumber(${Math.random()})`)
+      .catch(console.error)
+  })
+
+  browserWindow.loadURL(require('../resources/webview.html'))
+}
