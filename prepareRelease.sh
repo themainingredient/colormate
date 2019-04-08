000a0a21dbfe7f33cb2c689a20c5501485cf5d93
diogo batista <diogo@themainingredient.co>
2019-04-08 16:56:34 +0100

    added release expression

--- prepareRelease.sh
+++ prepareRelease.sh
@@ -3,7 +3,7 @@
 echo 'Building APP with version: ' $1
 echo 'Building app for branch: ' $2
 
-if [[ "$2" =~ "feature/ship-to-production"$ ]]
+if [[ "$2" =~ "release/"$ ]]
 then
   jq --arg h "$1" '.version=$h' package.json | sponge package.json
   jq --arg h "$1" '.version=$h' src/manifest.json | sponge src/manifest.json
