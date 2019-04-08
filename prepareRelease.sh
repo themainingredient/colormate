e1fbf889068c383c58032e752ea86ff5069ff397
diogo batista <diogo@themainingredient.co>
2019-04-08 16:01:37 +0100

    regex in pre-release

--- prepareRelease.sh
+++ prepareRelease.sh
@@ -3,12 +3,20 @@
 echo 'Building APP with version: ' $1
 echo 'Building app for branch: ' $2
 
-if [ "$2" = "feature/ship-to-production" ] # TODO: change this to regex matching release branches
+if [ "$2" =~ "feature/ship-to-production" ]
 then
   jq --arg h "$1" '.version=$h' package.json | sponge package.json
   jq --arg h "$1" '.version=$h' src/manifest.json | sponge src/manifest.json
   touch .env
   echo REACT_APP_VERSION=$1-beta >> .env
+fi
+
+if [ "$2" =~ "master" ]
+then
+  jq --arg h "$1" '.version=$h' package.json | sponge package.json
+  jq --arg h "$1" '.version=$h' src/manifest.json | sponge src/manifest.json
+  touch .env
+  echo REACT_APP_VERSION=$1 >> .env
 fi
 
 
