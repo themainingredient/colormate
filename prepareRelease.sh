4a7bfb80a042303e8aaa34e9b1d0e7c0d2692fc7
diogo batista <diogo@themainingredient.co>
2019-04-08 12:59:06 +0100

    read version from package json

--- prepareRelease.sh
+++ prepareRelease.sh
@@ -7,8 +7,6 @@ if [ "$2" = "feature/ship-to-production" ] # TODO: change this to regex matching
 then
   jq --arg h "$1" '.version=$h' package.json | sponge package.json
   jq --arg h "$1" '.version=$h' src/manifest.json | sponge src/manifest.json
-  echo 'export PATH=~/bin:$PATH' >> $BASH_ENV
-  echo 'export REACT_APP_VERSION=$(cat $1)' >> $BASH_ENV
 fi
 
 
