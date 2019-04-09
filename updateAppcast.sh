a9a4bb1f75e304fa72ee74160492812a4db7476d
diogo batista <diogo@themainingredient.co>
2019-04-09 14:14:31 +0100

    update appcast file

---
+++ updateAppcast.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+echo 'Version: ' $1
+echo 'URL: ' $2
+
+xmlstarlet ed -L -s "/rss/channel" -t elem -n itemTmp -v "" \
+            -s //itemTmp -t elem -n title -v "Version v$1" \
+            -s //itemTmp -t elem -n enclosureTmp -v "" \
+            -s //enclosureTmp -t attr -n url -v $2 \
+            -s //enclosureTmp -t attr -n sparkle:version -v $1 \
+            -r //enclosureTmp -v enclosure \
+            -r //itemTmp -v item \
+            .appcast.xml
+
+cat .appcast.xml
