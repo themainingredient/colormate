2ab8983a8162a122fa674e3ef8528189c959d427
Michele <michele@themainingredient.co>
2019-04-10 15:35:19 +0200

    no colors found page

--- resources/webview.js
+++ resources/webview.js
@@ -1,5 +1,6 @@
 import './app';
 
+// TODO: disable the listener for dev env
 // Disable the context menu to have a more native feel
 document.addEventListener('contextmenu', (e) => {
   e.preventDefault();
