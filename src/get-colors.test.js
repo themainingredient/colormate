817b7ae3a89a8b17d8bddc3066e2028ecd1fedf6
Michele <michele@themainingredient.co>
2019-04-11 17:36:18 +0200

    failing test comment

--- src/get-colors.test.js
+++ src/get-colors.test.js
@@ -1,6 +1,7 @@
 import getColors from './get-colors';
 import getColorsResult from './__mocks__/getColorsResult.json';
 
+// TODO: failing test, debug!
 describe('get-colors', () => {
   test.skip('returns an object with all the colors that were found', () => {
     expect(getColors()).toEqual(getColorsResult);
