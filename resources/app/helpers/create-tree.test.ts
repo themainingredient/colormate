9afa6087aabf3b6ca07ecd928c11074f9e1ee766
jeroennknol <jeroen-knol@live.nl>
2019-04-15 14:17:41 +0200

    Added some 'any' interfaces so the project would compile

--- resources/app/helpers/create-tree.test.ts
+++ resources/app/helpers/create-tree.test.ts
@@ -1,160 +1,163 @@
-import { colorsObjectToArray } from './create-tree';
+import { colorsObjectToArray } from './create-tree'; // eslint-disable-line import/no-unresolved
 
 describe('createTreeStructure', () => {
-    let input: any;
-    let output: any;
-    afterEach(() => { expect(colorsObjectToArray(input)).toEqual(output); });
+  let input: any;
+  let output: any;
+  afterEach(() => {
+    expect(colorsObjectToArray(input)).toEqual(output);
+  });
 
-    test('transform colors object to an array', () => {
-      input = {
-        red: [],
-        yellow: [],
-      };
+  test('transform colors object to an array', () => {
+    input = {
+      red: [],
+      yellow: [],
+    };
 
-      output = [
-        { color: 'red', layers: [] },
-        { color: 'yellow', layers: [] },
-      ];
-    });
+    output = [{ color: 'red', layers: [] }, { color: 'yellow', layers: [] }];
+  });
 
-    test('should convert a basic tree', () => {
-      input = {
-        red: [{
+  test('should convert a basic tree', () => {
+    input = {
+      red: [
+        {
           name: 'Rectangle1',
-        }],
-      };
+        },
+      ],
+    };
 
-      output = [{
+    output = [
+      {
         color: 'red',
-        layers: [{
-          name: 'Rectangle1',
-        }],
-      }];
-    });
+        layers: [
+          {
+            name: 'Rectangle1',
+          },
+        ],
+      },
+    ];
+  });
 
-    test('should convert a tree with one parent', () => {
-      input = {
-        red: [{
+  test('should convert a tree with one parent', () => {
+    input = {
+      red: [
+        {
           name: 'Rectangle1',
-          parents: [
-            { name: 'Page' },
-          ],
-        }],
-      };
+          parents: [{ name: 'Page' }],
+        },
+      ],
+    };
 
-      output = [{
+    output = [
+      {
+        color: 'red',
+        layers: [
+          {
+            name: 'Page',
+            children: [{ name: 'Rectangle1' }],
+          },
+        ],
+      },
+    ];
+  });
+
+  test('should convert a tree with 1 layer with 2 parents', () => {
+    input = {
+      red: [
+        {
+          name: 'Rectangle1',
+          parents: [{ name: 'Page' }, { name: 'Artboard' }],
+        },
+      ],
+    };
+
+    output = [
+      {
         color: 'red',
         layers: [
           {
             name: 'Page',
             children: [
-              { name: 'Rectangle1' },
+              {
+                name: 'Artboard',
+                children: [{ name: 'Rectangle1' }],
+              },
             ],
           },
         ],
-      }];
-    });
+      },
+    ];
+  });
 
-    test('should convert a tree with 1 layer with 2 parents', () => {
-      input = {
-        red: [{
+  test('should convert a tree with 2 layers with different parents', () => {
+    input = {
+      red: [
+        {
           name: 'Rectangle1',
-          parents: [
-            { name: 'Page' },
-            { name: 'Artboard' },
-          ],
-        }],
-      };
-
-      output = [{
-        color: 'red',
-        layers: [
-          {
-            name: 'Page',
-            children: [{
-              name: 'Artboard',
-              children: [
-                { name: 'Rectangle1' },
-              ],
-            }],
-          },
-        ],
-      }];
-    });
-
-    test('should convert a tree with 2 layers with different parents', () => {
-      input = {
-        red: [{
-          name: 'Rectangle1',
-          parents: [
-            { name: 'Page' },
-            { name: 'Artboard' },
-          ],
-        }, {
+          parents: [{ name: 'Page' }, { name: 'Artboard' }],
+        },
+        {
           name: 'Rectangle2',
-          parents: [
-            { name: 'Page2' },
-            { name: 'Artboard2' },
-          ],
-        }],
-      };
+          parents: [{ name: 'Page2' }, { name: 'Artboard2' }],
+        },
+      ],
+    };
 
-      output = [{
+    output = [
+      {
         color: 'red',
         layers: [
           {
             name: 'Page',
-            children: [{
-              name: 'Artboard',
-              children: [
-                { name: 'Rectangle1' },
-              ],
-            }],
+            children: [
+              {
+                name: 'Artboard',
+                children: [{ name: 'Rectangle1' }],
+              },
+            ],
           },
           {
             name: 'Page2',
-            children: [{
-              name: 'Artboard2',
-              children: [
-                { name: 'Rectangle2' },
-              ],
-            }],
+            children: [
+              {
+                name: 'Artboard2',
+                children: [{ name: 'Rectangle2' }],
+              },
+            ],
           },
         ],
-      }];
-    });
+      },
+    ];
+  });
 
-    test.skip('should convert a tree with 2 layers with the same parents', () => {
-      input = {
-        red: [{
+  test.skip('should convert a tree with 2 layers with the same parents', () => {
+    input = {
+      red: [
+        {
           name: 'Rectangle1',
-          parents: [
-            { name: 'Page' },
-            { name: 'Artboard' },
-          ],
-        }, {
+          parents: [{ name: 'Page' }, { name: 'Artboard' }],
+        },
+        {
           name: 'Rectangle2',
-          parents: [
-            { name: 'Page' },
-            { name: 'Artboard' },
-          ],
-        }],
-      };
+          parents: [{ name: 'Page' }, { name: 'Artboard' }],
+        },
+      ],
+    };
 
-      output = [{
+    output = [
+      {
         color: 'red',
         layers: [
           {
             name: 'Page',
-            children: [{
-              name: 'Artboard',
-              children: [
-                { name: 'Rectangle1' },
-                { name: 'Rectangle2' },
-              ],
-            }],
+            children: [
+              {
+                name: 'Artboard',
+                children: [{ name: 'Rectangle1' }, { name: 'Rectangle2' }],
+              },
+            ],
           },
         ],
-      }];
-    });
+      },
+    ];
   });
+});
--- resources/app/helpers/create-tree.ts
+++ resources/app/helpers/create-tree.ts
@@ -1,57 +1,54 @@
-const mapLevelArrayToTreeObject = (inputArray) => {
-    if (inputArray.length === 1) {
-      return ({
-        name: inputArray[0].name,
-      });
-    }
-  
-    return ({
+const mapLevelArrayToTreeObject = (inputArray: any): any => {
+  if (inputArray.length === 1) {
+    return {
       name: inputArray[0].name,
-      children: [mapLevelArrayToTreeObject(inputArray.splice(1))],
-    });
+    };
+  }
+
+  return {
+    name: inputArray[0].name,
+    children: [mapLevelArrayToTreeObject(inputArray.splice(1))],
   };
-  
-  const mapInstanceToLevelsArray = (instance) => {
-    if ('parents' in instance && instance.parents.length) {
-      return [
-        ...instance.parents,
-        { name: instance.name },
-      ];
-    }
-  
-    return [{ name: instance.name }];
-  };
-  
-  const createTreeObjectFromLevelsArray = (levelsArray) => {
-    if (levelsArray.length === 1) {
-      return ({
-        name: levelsArray[0].name,
-      });
-    }
-  
-    return mapLevelArrayToTreeObject(levelsArray);
-  };
-  
+};
+
+const mapInstanceToLevelsArray = (instance: any): any => {
+  if ('parents' in instance && instance.parents.length) {
+    return [...instance.parents, { name: instance.name }];
+  }
+
+  return [{ name: instance.name }];
+};
+
+// const createTreeObjectFromLevelsArray = (levelsArray: any): any => {
+//   if (levelsArray.length === 1) {
+//     return {
+//       name: levelsArray[0].name,
+//     };
+//   }
+
+//   return mapLevelArrayToTreeObject(levelsArray);
+// };
+
 //   export const colorsObjectToArray = (colorsObject) => {
 //     const output = Object.entries(colorsObject).map(([color, instances]) => ({
 //       color,
 //       layers: instances.reduce((acc, instance) => {
 //         const levelsArray = mapInstanceToLevelsArray(instance);
-  
+
 //         if (acc.length === 0) {
 //           return [
 //             ...acc,
 //             createTreeObjectFromLevelsArray(levelsArray),
 //           ];
 //         }
-  
+
 //         // [
 //         //   { name: 'Rectangle2' }
 //         // ]
-  
+
 //         const recursion = (accumulator, arr) => {
 //           if (!arr.length) { return; }
-  
+
 //           const matchingName = accumulator.find((layer) => { return layer.name === arr[0].name; });
 //           if (matchingName) {
 //             console.log('TRUE');
@@ -64,10 +61,9 @@ const mapLevelArrayToTreeObject = (inputArray) => {
 //             return createTreeObjectFromLevelsArray(arr);
 //           }
 //         };
-  
+
 //         recursion(acc, levelsArray);
-  
-  
+
 //         // given a level
 //         // loop acc array
 //         // find a matching name
@@ -75,22 +71,23 @@ const mapLevelArrayToTreeObject = (inputArray) => {
 //         // if found, go into the item.children and iterate on the remaining levels
 //       }, []),
 //     }));
-  
+
 //     console.log('helpers.js - output ', JSON.stringify(output, null, 2));
-  
+
 //     return output;
 //   };
 
-export const colorsObjectToArray = (colorsObject) => {
+export const colorsObjectToArray = (colorsObject: any[]): any => {
   const output = Object.entries(colorsObject).map(([color, instances]) => ({
+    //
     color,
-    layers: instances.map((instance) => {
+    layers: instances.map((instance: any) => {
       const levelsArray = mapInstanceToLevelsArray(instance);
 
       if (levelsArray.length === 1) {
-        return ({
+        return {
           name: levelsArray[0].name,
-        });
+        };
       }
 
       const returnObj = mapLevelArrayToTreeObject(levelsArray);
@@ -99,7 +96,7 @@ export const colorsObjectToArray = (colorsObject) => {
     }),
   }));
 
-//   console.log('helpers.js - output ', JSON.stringify(output, null, 2));
+  //   console.log('helpers.js - output ', JSON.stringify(output, null, 2));
 
   return output;
 };
