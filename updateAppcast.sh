86dff9ec0c717765aa7e02a6f42c9beec4953e77
diogo batista <diogo@themainingredient.co>
2019-04-18 16:19:12 +0200

    generate appcast url

--- updateAppcast.sh
+++ updateAppcast.sh
@@ -3,10 +3,12 @@
 echo 'Version: ' $1
 echo 'URL: ' $2
 
+URL=$2$1
+
 xmlstarlet ed -L -s "/rss/channel" -t elem -n itemTmp -v "" \
             -s //itemTmp -t elem -n title -v "Version v$1" \
             -s //itemTmp -t elem -n enclosureTmp -v "" \
-            -s //enclosureTmp -t attr -n url -v $2 \
+            -s //enclosureTmp -t attr -n url -v $URL \
             -s //enclosureTmp -t attr -n sparkle:version -v $1 \
             -r //enclosureTmp -v enclosure \
             -r //itemTmp -v item \
