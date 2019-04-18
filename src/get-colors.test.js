2154878072229d32f394e3df1fb082be5567b78b
Michele <michele@themainingredient.co>
2019-04-18 11:46:04 +0200

    updated color results because of updated func

--- src/get-colors.test.js
+++ src/get-colors.test.js
@@ -1,9 +1,8 @@
 import getColors from './get-colors';
 import getColorsResult from './__mocks__/getColorsResult.json';
 
-// TODO: failing test, debug!
 describe('get-colors', () => {
-  test.skip('returns an object with all the colors that were found', () => {
+  test('returns an object with all the colors that were found', () => {
     expect(getColors()).toEqual(getColorsResult);
   });
 });
