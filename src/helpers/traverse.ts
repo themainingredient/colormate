13da1b1412a576654466bcf0ff9c54e1d0e45eee
Michele <michele@themainingredient.co>
2019-04-24 16:40:33 +0200

    traverse typings

--- src/helpers/traverse.ts
+++ src/helpers/traverse.ts
@@ -1,7 +1,7 @@
-import { Layer } from './../models/layer.model';
 import { getParents } from './get-colors';
+import { Page, Layer } from 'sketch';
 
-export const traverse = (layer: Layer, layers: any[] = [], parents: any[] = []) => {  
+export const traverse = (layer: Page | Layer, layers: any[] = [], parents: any[] = []): any => {  
   if (('layers' in layer) && layer.layers!.length) {
     layer.layers!.forEach((subLayer: any) => (
       traverse(subLayer, layers, getParents(parents, { id: layer.id, type: layer.type, name: layer.name }))
