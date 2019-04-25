73530ceb3f2c16a6891afe25f8f9406a5db695fb
Michele <michele@themainingredient.co>
2019-04-25 17:16:56 +0200

    get colors test with virtual mocking

--- __mocks__/sketch.js
+++
@@ -1,7 +0,0 @@
-import TreeMock from '../src/__mocks__/MockSketchDocument.json';
-
-module.exports = {
-  getSelectedDocument: () => {
-    return TreeMock;
-  },
-};
--- src/get-colors.test.ts
+++ src/get-colors.test.ts
@@ -1,9 +1,47 @@
 import getColors from './get-colors';
 import getColorsResult from './__mocks__/getColorsResult.json';
+import TreeMock from './__mocks__/MockSketchDocument.json';
+import * as fromGetColorHelper from './helpers/get-colors';
+import {getSelectedDocument} from 'sketch';
 
-//TODO: review with changes
-describe.skip('get-colors', () => {
-  test('returns an object with all the colors that were found', () => {
-    expect(getColors()).toEqual(getColorsResult);
+jest.mock('sketch', () => ({
+  getSelectedDocument: jest.fn()
+}), {virtual: true});
+
+describe('getColors', () => {
+  describe('if layers are not selected', () => {
+    beforeAll(() => {
+      (getSelectedDocument as jest.Mock).mockImplementation(() => TreeMock);
+    })
+
+    test('returns an object with all the colors that were found', () => {
+      expect(getColors()).toEqual(getColorsResult);
+    });
+  })
+
+  describe('if layers are selected', () => {
+    const selectedLayers = {
+      isEmpty: false,
+      layers: [],
+    }
+
+    beforeAll(() => {
+      (getSelectedDocument as jest.Mock).mockImplementation(() => ({
+            selectedLayers
+      }));
+    })
+
+    test('should call getPagesWithSelectedLayers with the selectedLayers', () => {
+      const spy = jest.spyOn(fromGetColorHelper, 'getPagesWithSelectedLayers');
+
+      getColors();
+
+      expect(spy).toHaveBeenCalledTimes(1);
+      expect(spy).toHaveBeenCalledWith(selectedLayers);
+
+      spy.mockRestore();
+    });
   });
 });
+
+jest.unmock('sketch');
