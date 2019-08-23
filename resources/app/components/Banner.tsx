cc9cc1db1061e55497b63baa3be09fda0b901d14
Michele <michele@themainingredient.co>
2019-08-23 12:44:46 +0200

    banner design

--- resources/app/components/Banner.tsx
+++ resources/app/components/Banner.tsx
@@ -2,13 +2,55 @@ import React, {useState, useEffect} from 'react';
 import styled from 'styled-components';
 import Globals from '../Global.styles';
 import { openUrlInBrowser } from '../helpers/window';
-// TODO: style banner
 
-const {colors} = Globals;
+const {colors, fonts} = Globals;
 
+// TODO: add url from env
 const BannerWrapper = styled.div`
-    background-color: ${colors.LightGrey};
+    background-color: ${colors.Navy};
+    height: 100px;
+    width: 100%;
+    padding: 16px 12px;
 `;
+
+const Text = styled.p`
+    color: ${colors.White};
+    font-family: ${fonts.SFPro.bold};
+    font-size: 16px;
+    letter-spacing: 0px;
+    text-align: center;
+`;
+
+const ButtonsWrapper = styled.div`
+    display: flex;
+    justify-content: space-between;
+    padding: 12px 16px;
+`;
+
+const Button = styled.button`
+    padding: 10px 16px;
+    background: ${colors.TMIBlue};
+    border-radius: 4.53px;
+
+    color: ${colors.Cyan};
+    font-size: 14px;
+    font-family: ${fonts.SFPro.heavy};
+    text-align: center;
+    border: none;
+
+    &:hover {
+        background: ${colors.TMIBlueDark};
+    }
+
+    &:active {
+        color: ${colors.CyanDark};
+    }
+`;
+
+const Emoji = styled.span`
+    font-size: 33px;
+    height: 0px; /* remove default extra whitespace from emoji */
+`
 
 export const Banner  = () => {
     const [isBannerVisible, showBanner] = useState(false);
@@ -45,9 +87,14 @@ export const Banner  = () => {
 
     return (
         <BannerWrapper>
-            <button onClick={handleYes}>yes</button>
-            <button onClick={handleNo}>no</button>
-            <button onClick={handleAskAgain}>postpone</button>
+            <Text>Help us with a 2 minute questionnaire?</Text>
+            
+            <ButtonsWrapper>
+                <Emoji>🎱</Emoji> 
+                <Button onClick={handleYes}>Yes</Button>
+                <Button onClick={handleNo}>No</Button>
+                <Button onClick={handleAskAgain}>Ask again later</Button>
+            </ButtonsWrapper>
         </BannerWrapper>
     )
 }
