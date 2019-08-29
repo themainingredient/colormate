ab913474ee41a0aabd37eb3d0cfdf124d8ec87e3
Michele <michele@themainingredient.co>
2019-08-29 13:43:31 +0200

    lint

--- src/helpers/get-page.test.ts
+++ src/helpers/get-page.test.ts
@@ -1,40 +1,40 @@
 
-import { Layer } from 'sketch';
-import {getPage} from './get-page';
+import { Layer } from 'sketch'; // eslint-disable-line import/no-unresolved
+import { getPage } from './get-page';
 import { LayerType } from '../../enums/layer-type.enum';
 
 describe('getPage', () => {
-    test('should return the layer if is a page', () => {
-        const layer = {
-            type: LayerType.page
-        }
+  test('should return the layer if is a page', () => {
+    const layer = {
+      type: LayerType.page,
+    };
 
-        expect(getPage(layer as Layer)).toBe(layer);
-    });
+    expect(getPage(layer as Layer)).toBe(layer);
+  });
 
-    test('should return the first parent page', () => {
-        const page = {
-            type: LayerType.page,
-            parent: {
-                type: LayerType.document
-            }
-        }
-        const layer = {
-            type: LayerType.shapePath,
-            parent: {
-                type: LayerType.artboard,
-                parent: page
-            }
-        }
+  test('should return the first parent page', () => {
+    const page = {
+      type: LayerType.page,
+      parent: {
+        type: LayerType.document,
+      },
+    };
+    const layer = {
+      type: LayerType.shapePath,
+      parent: {
+        type: LayerType.artboard,
+        parent: page,
+      },
+    };
 
-        expect(getPage(layer as Layer)).toBe(page);
-    });
+    expect(getPage(layer as Layer)).toBe(page);
+  });
 
-    test('should throw an error if the layer has no parent page', () => {
-        const layer = {
-            type: LayerType.shapePath
-        }
+  test('should throw an error if the layer has no parent page', () => {
+    const layer = {
+      type: LayerType.shapePath,
+    };
 
-        expect(() => getPage(layer as Layer)).toThrow()
-    })
+    expect(() => getPage(layer as Layer)).toThrow();
+  });
 });
--- src/helpers/get-page.ts
+++ src/helpers/get-page.ts
@@ -1,14 +1,14 @@
-import { Page, Layer } from 'sketch';
+import { Page, Layer } from 'sketch'; // eslint-disable-line import/no-unresolved
 import { LayerType } from '../../enums/layer-type.enum';
 
 export const getPage = (layer: Layer): Page => {
-    if (layer.type === LayerType.page) {
-        return layer;
-    }
+  if (layer.type === LayerType.page) {
+    return layer;
+  }
 
-    if (!layer.parent) {
-        throw new Error(`Layer is not child on any page! ${JSON.stringify(layer, null, 2)}`);
-    }
+  if (!layer.parent) {
+    throw new Error(`Layer is not child on any page! ${JSON.stringify(layer, null, 2)}`);
+  }
 
-    return getPage(layer.parent as Layer);
-}
+  return getPage(layer.parent as Layer);
+};
