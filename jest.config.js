44a7085875b63189aecd2304797dd594ff95453c
Michele <michele@themainingredient.co>
2019-09-02 13:48:22 +0200

    tsconfig test

--- jest.config.js
+++ jest.config.js
@@ -56,9 +56,7 @@ module.exports = {
   // A set of global variables that need to be available in all test environments
   globals: {
     'ts-jest': {
-      diagnostics: {
-        warnOnly: true,
-      },
+      tsConfig: 'tsconfig.test.json',
     },
   },
 
